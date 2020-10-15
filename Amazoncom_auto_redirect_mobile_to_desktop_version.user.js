1a7737b35a874322ac0eaf873541dad61bc2b5ae
V@no <git@vano.org>
2020-10-14 23:22:59 -0400

    no message

--- Amazoncom_auto_redirect_mobile_to_desktop_version.user.js
+++ Amazoncom_auto_redirect_mobile_to_desktop_version.user.js
@@ -1,16 +1,41 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @id             amazon.com_mobile2desktop
 // @name           Amazon.com auto redirect mobile to desktop version
-// @version        1.1
+// @version        1.3.2
 // @namespace      V@no
 // @author         V@no
 // @description    Automatically redirect from mobile version to desktop version of the page.
-// @include        http://www.amazon.com/gp/aw/d/*
-// @include        http://amazon.com/gp/aw/d/*
-// @include        https://www.amazon.com/gp/aw/d/*
-// @include        https://amazon.com/gp/aw/d/*
+// @include        http://amazon.com/*
+// @include        http://www.amazon.com/*
+// @include        https://amazon.com/*
+// @include        https://www.amazon.com/*
 // @grant          none
 // @run-at         document-start
 // ==/UserScript==
 
-window.location.replace(window.location.toString().replace(/^(http:\/\/(www\.)?amazon\.com\/)gp\/aw\/d\/(.*)/ig, "$1dp/$3"));
+//redirect mobile to desktop
+(function()
+{
+	let log = console.log.bind(console);
+	if (location.pathname.match(/^\/gp\/aw\//i))
+	{
+		window.location.replace(window.location.toString().replace(/^(https?:\/\/(www\.)?amazon\.com\/)gp\/aw\/(d\/)?(.*)/ig, "$1dp/$4"));
+	}
+	else
+	{
+		//redirect to charity amazon
+		if (!location.hostname.match(/smile\.amazon\.com/i) && location.href.indexOf("signin") == -1)
+		{
+			let smile = window.location.toString().replace(/^(https?:\/\/)([^\/]+)\/(.*)/ig, "$1smile.amazon.com/$3");
+			if (location.search.indexOf("pldnSite") == -1)
+			{
+log("Redirecting to smile.amazon.com");
+				window.location.replace(smile);
+			}
+			else
+			{
+log("Can't redirect to smile.amazon.com due to pldnSite")
+			}
+		}
+	}
+})();
